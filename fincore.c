0f22e9b23ebabe12559ce55da04068d46e3d95a8
Martin A. Brown <mabrown@solidus.wonderfrog.net>
2008-01-05 14:17:16 -0600

    Initial commit with working build.

---
+++ fincore.c
@@ -0,0 +1,211 @@
+/* fincore.c - report which blocks of a file are in the page buffer cache
+   Copyright (C) 1991-1997, 1999-2004 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Martin A. Brown <mabrown@renesys.com> */
+
+#include <config.h>
+
+#include <stdio.h>     /* perror, fprintf, stderr, printf */
+#include <errno.h>
+#include <getopt.h>
+#include <fcntl.h>     /* fcntl, open */
+#include <stdlib.h>    /* exit, calloc, free */
+#include <string.h>    /* strerror */
+#include <unistd.h>    /* sysconf, close */
+#include <sys/stat.h>  /* stat, fstat */
+#include <sys/types.h> /* size_t */
+#include <sys/mman.h>  /* mincore */
+
+/* The official name of this program (e.g., no `g' prefix).  */
+#define PROGRAM_NAME "fincore"
+#define AUTHORS "Martin A. Brown"
+
+#define FINC_NO_HEADER        ( 1 << 0 );
+#define FINC_SUMMARY_ONLY     ( 1 << 1 );
+#define FINC_SUMMARY          ( 1 << 2 );
+#define FINC_LIST_PAGES       ( 1 << 3 );
+
+struct a_options {
+  char                   *files_from;
+  int                     reporting_mode;
+};
+
+struct a_options o = {
+     .files_from      = NULL,
+     .reporting_mode  = 0
+};
+
+/* Usage Macros */
+#define MSG( D, F, ...) fprintf( D, F , ##__VA_ARGS__ )
+#define FATAL( F, ...) { MSG( stderr, F, ##__VA_ARGS__ ); exit( EXIT_FAILURE ); }
+#define USAGE_FATAL( F, ... ) { short_usage( EXIT_FAILURE ); FATAL( F, ##__VA_ARGS__ ); }
+
+
+/* The name this program was run with. */
+char *program_name;
+char *program_btime     = __DATE__" "__TIME__;
+char *program_copyright = "copyright Renesys"  ;
+const float program_version = 0.10f;
+
+
+void
+short_usage(int ret)
+{
+  fprintf( ( ret ? stderr : stdout ),
+    "usage: %s [ OPTIONS ] [ <file> ... ]\nTry \"%s --help\" for more.\n",
+    program_name, program_name );
+}
+
+void
+long_usage(int ret)
+{
+  short_usage( ret );
+  fprintf( ( ret ? stderr : stdout ),
+    "\n"
+    "Options (defaults in parentheses):\n"
+    "  -h, --help, --usage       display this help and exit.\n"
+    "  -V, --version             display version information and exit.\n"
+    "  -v, --verbose             produce listing of blocks/pages in core\n"
+    "  -f, --files-from <file>   read list of files from <file>\n"
+    "  -N, --no-header           suppress initial header line\n"
+    "  -s, --summary             include a summary at the bottom\n"
+    "  -S, --summary-only        produce only totals\n"
+    "\n"
+         );
+  exit( ret );
+}
+
+void
+print_version(int ret)
+{
+  fprintf( ( ret ? stderr : stdout ), "%s\n%s-%.2f (compiled %s)\n",
+           program_copyright, program_name, program_version, program_btime );
+  exit( ret );
+}
+
+static void
+parse_options( int ac, char **av )
+{
+  extern char *optarg;
+  extern int optind;
+  int c;
+
+  static struct option const longopts[] =
+  {
+    {"no-header", no_argument, NULL, 'N'},
+    {"files-from", required_argument, NULL, 'f'},
+    {"summary", no_argument, NULL, 's'},
+    {"summary-only", no_argument, NULL, 'S'},
+    {"verbose", no_argument, NULL, 'v'},
+    {"version", no_argument, NULL, 'V'},
+    {"help", no_argument, NULL, 'h'},
+    {"usage", no_argument, NULL, 'h'},
+    {NULL, 0, NULL, 0}
+  };
+
+  opterr = 0; /* Keep getopt quiet. */
+
+  while ( -1 != (c = getopt_long (ac, av, "+hVvNSsf:", longopts, NULL)) ) 
+  {
+    switch (c)
+    {
+      case '?': USAGE_FATAL( "Unrecognized option:  \"%s\"\n" ); break;
+      case 'f': o.files_from      = optarg;               break;
+      case 'N': o.reporting_mode |= FINC_NO_HEADER;       break;
+      case 'S': o.reporting_mode  = FINC_SUMMARY_ONLY ;   break;
+      case 's': o.reporting_mode |= FINC_SUMMARY;         break;
+      case 'v': o.reporting_mode |= FINC_LIST_PAGES;      break;
+      case 'h': long_usage( EXIT_SUCCESS );               break;
+      case 'V': print_version( EXIT_SUCCESS );            break;
+      default: USAGE_FATAL("");  break;
+    }
+  }
+
+}
+
+/* fincore -
+ */
+void
+fincore(char *filename)
+{
+   int fd;
+   struct stat st;
+   void *pa = (char *)0;
+   unsigned char *vec = (unsigned char *)0;
+   register size_t n = 0;
+   size_t pageSize = getpagesize();
+   register size_t pageIndex;
+   fd = open(filename, 0);
+   if (0 > fd) {
+      perror("open");
+      return;
+   }
+
+   if (0 != fstat(fd, &st)) {
+      perror("fstat");
+      close(fd);
+      return;
+   }
+
+   pa = mmap((void *)0, st.st_size, PROT_NONE, MAP_SHARED, fd, 0);
+   if (MAP_FAILED == pa) {
+      perror("mmap");
+      close(fd);
+      return;
+   }
+
+   /* vec = calloc(1, 1+st.st_size/pageSize); */
+   vec = calloc(1, (st.st_size+pageSize-1)/pageSize);
+   if ((void *)0 == vec) {
+      perror("calloc");
+      close(fd);
+      return;
+   }
+
+   if (0 != mincore(pa, st.st_size, vec)) {
+      /* perror("mincore"); */
+      fprintf(stderr, "mincore(%p, %lu, %p): %s\n",
+              pa, (unsigned long)st.st_size, vec, strerror(errno));
+      free(vec);
+      close(fd);
+      return;
+   }
+
+   /* handle the results */
+   for (pageIndex = 0; pageIndex <= st.st_size/pageSize; pageIndex++) {
+      if (vec[pageIndex]&1) {
+         printf("%lu\n", (unsigned long)pageIndex);
+      }
+   }
+
+   free(vec);
+   vec = (unsigned char *)0;
+
+   munmap(pa, st.st_size);
+   close(fd);
+   return;
+}
+
+int
+main (int argc, char ** argv)
+{
+  //int optc;
+  program_name = PROGRAM_NAME; /* argv[0]; */
+  parse_options( argc, argv );
+
+}
+
